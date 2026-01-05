23775c1d8a80ba1d8720f0c646a984082d72a79c
kunisuzaki <kuni.suzaki@gmail.com>
2026-01-05 14:52:11 +0900

    Sakura Sample

---
+++ SEV-SNP/Sakura/sakura-snp-sra-by-go-sev-guest.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# Sakura SEV-SNP Remote Attestation using go-sev-guest (Report Generation and Verification)
+
+set -e
+set -o pipefail
+
+# Please change the following variable to your own environment
+GUEST_POLICY=0x030000
+
+# Prepare Report Data
+echo "Prepare 64-byte report data"
+openssl rand 64 > request-file.bin
+xxd -p request-file.bin | tr -d '\n' > request-file.txt
+
+# Request SEV-SNP AR
+echo "Request SEV-SNP AR"
+sudo go-sev-guest-attest \
+	-inform bin \
+	-infile request-file.bin \
+	-outform bin \
+	-out report.bin \
+	-vmpl 2
+
+# Verify SEV-SNP AR
+echo "Verify SEV-SNP AR"
+set +e
+go-sev-guest-check \
+	-inform bin \
+	-in report.bin \
+	-guest_policy $GUEST_POLICY \
+	-provisional true \
+	-report_data "$(< request-file.txt)" \
+	-vmpl 2
+CHECK_EXIT_CODE=$?
+set -e
+
+# Check the exit code
+case $CHECK_EXIT_CODE in
+0) echo "Verification Success";;
+1) echo "Verification Failure due to tool misuse"; exit 1 ;;
+2) echo "Verification Failure due to invalid signature"; exit 1 ;;
+3) echo "Verification Failure due to certificate fetch failure"; exit 1 ;;
+4) echo "Verification Failure due to certificate revocation list download failure"; exit 1 ;;
+5) echo "Verification Failure due to policy"; exit 1 ;;
+*) echo "Verification Unknown error"; exit 1 ;;
+esac
+
+# Parse SEV-SNP AR
+echo "Parse SEV-SNP AR"
+go-sev-guest-show -inform bin -in report.bin -outform textproto
---
+++ SEV-SNP/Sakura/sakura-snp-sra-by-snpguest.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Sakura SEV-SNP Remote Attestation using snpguest (Report Generation and Verification)
+
+set -e
+
+# Please change the following variable to your own environment
+VMPL_VALUE=2
+
+# Prepare Report Data
+echo "Prepare 64-byte report data"
+openssl rand 64 > request-file.bin
+
+# Request SEV-SNP AR
+echo "Request SEV-SNP AR"
+sudo snpguest report report.bin request-file.bin -v $VMPL_VALUE
+
+# Fetch Cert Chain ARK->ASK->VCEK from AMD KDS
+echo "Fetch Cert Chain ARK->ASK->VCEK from AMD KDS"
+snpguest fetch ca pem certs -r report.bin -e vcek
+snpguest fetch vcek pem certs report.bin
+
+# Verify Cert Chain ARK->ASK->VCEK
+echo "Verify Cert Chain ARK->ASK->VCEK"
+snpguest verify certs certs
+
+# Verify Signature VCEK->SEV-SNP AR
+echo "Verify Signature VCEK->SEV-SNP AR"
+snpguest verify attestation certs report.bin
+
+# Extract Report Data from SEV-SNP AR
+echo "Extract Report Data from SEV-SNP AR"
+REPORT_DATA_OFFSET=80
+REPORT_DATA_SIZE=64
+
+dd if=report.bin \
+  skip=$REPORT_DATA_OFFSET \
+  bs=1 \
+  count=$REPORT_DATA_SIZE \
+  of=report-data.bin \
+  status=none
+
+# Verify Report Data
+echo "Verify Report Data"
+diff -s request-file.bin report-data.bin
+
+# Parse SEV-SNP AR
+echo "Parse SEV-SNP AR"
+snpguest display report report.bin
